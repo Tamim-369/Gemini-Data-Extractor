4dc4189ef781f1d6e20a0afd2e16681a31bacef7
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-01-03 09:49:18 -0500

    making it more functional by implementing custom url features

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import fs from "fs/promises";
 import inquirer from "inquirer";
 import puppeteer from "puppeteer";
 import { convert } from "html-to-text";
-async function takeScreenshot(url: string, path: string) {
+async function takeHTML(url: string) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle2" });
-
-  await page.screenshot({ path });
+  await page.goto(url);
+  const html = await page.evaluate(() => document.documentElement.outerHTML);
   await browser.close();
+  return html.toString();
 }
 async function htmlToText(html: string) {
   return convert(html);
@@ -21,27 +21,28 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-while (true) {
-  const prompt = await inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "input",
-        message: "Enter your prompt:",
-      },
-    ])
-    .then((answers) => answers);
-  //   await takeScreenshot(prompt.url, "screenshot.png");
-  //   const image = {
-  //     inlineData: {
-  //       data: await fs.readFile("screenshot.png", { encoding: "base64" }),
-  //       mimeType: "image/png",
-  //     },
-  //   };
-  const htmlFile = await fs.readFile("data.html", { encoding: "utf-8" });
-  const text = await htmlToText(htmlFile);
-  const result = await model.generateContent([prompt.input, text]);
-  const refinedResult = await result.toString().replace(/```|json/g, " ");
-  fs.writeFile("output.json", refinedResult);
-  console.log(result.response.text());
-}
+const prompt = `you are the best webscraper AI you are an artist in terms of getting raw html data and extracting or mining valuable data from html content now 
+please sir give me product data from the provided html the product data should be json and you should only give json data no other data. The json format will look 
+like this  {name: string, price:number, link: string}`;
+//   await takeScreenshot(prompt.url, "screenshot.png");
+//   const image = {
+//     inlineData: {
+//       data: await fs.readFile("screenshot.png", { encoding: "base64" }),
+//       mimeType: "image/png",
+//     },
+//   };
+const url = await inquirer.prompt({
+  type: "input",
+  name: "url",
+  message: "Enter the URL of the website you want to scrape",
+});
+const htmlFile = await takeHTML(url.url);
+const text = await htmlToText(htmlFile);
+const result = await model.generateContent([prompt, text]);
+const refinedResult = JSON.stringify(
+  JSON.parse(result.response.text().replace(/```|json/g, "")),
+  null,
+  2
+);
+fs.writeFile("output.json", refinedResult);
+console.log(result.response.text());
