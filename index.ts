17fdc58a7c04f85a9a7e4058a42a597d218d73e0
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-01-04 10:01:58 -0500

    solving max token issues

--- index.ts
+++ index.ts
@@ -6,15 +6,15 @@ import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { convert } from "html-to-text";
 import { takeHTML } from "./utils/takeHtml";
-import { htmlToText } from "html-to-text";
+
 dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY as string);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = `you are the best webscraper AI. You are an artist in terms of getting raw html or text data and extracting or mining valuable data from html or text content. now 
-please sir give me product data from the provided html or text the product data should be json and you should only give json data no other data. The json format will look 
+const prompt = `you are the best webscraper AI you are an artist in terms of getting raw html data and extracting or mining valuable data from html content now 
+please sir give me product data from the provided html the product data should be json and you should only give json data no other data. The json format will look 
 like this  {name: string, price:number, link: string}`;
 //   await takeScreenshot(prompt.url, "screenshot.png");
 //   const image = {
@@ -29,13 +29,16 @@ const url = await inquirer.prompt({
   message: "Enter the URL of the website you want to scrape",
 });
 const htmlFile = await takeHTML(url.url);
-const text = htmlToText(htmlFile);
-const result = await model.generateContent([prompt, text]);
-console.log(JSON.stringify(result));
+let result: any = await model.generateContent([prompt, htmlFile]);
+const jsonData = JSON.stringify(result);
+if (jsonData.includes("MAX_TOKENS") || jsonData.includes("parts")) {
+  result = JSON.parse(jsonData).response.candidates[0].content.parts[0].text;
+} else {
+  result = result.response.text;
+}
 const refinedResult = JSON.stringify(
-  JSON.parse(result.response.text().replace(/```|json/g, "")),
+  await JSON.parse(result.replace(/```|json/g, "")),
   null,
   2
 );
 fs.writeFile("output.json", refinedResult);
-console.log(result.response.text());
