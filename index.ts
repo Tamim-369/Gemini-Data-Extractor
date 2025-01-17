444fc7f2279b477622b7ec74f01c56e9dc7c81f5
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-01-17 08:26:44 -0500

    fixing some issues

--- index.ts
+++ index.ts
@@ -1,50 +1,27 @@
-import mongoose from "mongoose";
+import inquirer from "inquirer";
 import { ProductModel, type IProduct } from "./models/productModel";
-import { getProductData } from "./services/getProductData";
 import cron from "node-cron";
+import { productDataJob } from "./services/jobs/productDataJob";
+import { getProductData } from "./services/getProductData";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-// we can use node cron to run this continuesly
-// cron.schedule("0 0 */12 * * *",);
-const productDataJob = async () => {
-  const allProducts = await ProductModel.find();
-  if (!allProducts.length) {
-    return {
-      success: false,
-      message: "No product data found",
-    };
-  }
-  const uniqueProducts = new Map<string, any>();
-  await mongoose.connect(process.env.MONGODB_URI!);
-  allProducts.forEach((product: IProduct) => {
-    const key = `${product.name}-${product.price}`;
-    if (
-      !uniqueProducts.has(key) ||
-      new Date(product.createdAt) > new Date(uniqueProducts.get(key).createdAt)
-    ) {
-      uniqueProducts.set(key, product);
-    }
+await mongoose.connect(process.env.MONGODB_URI!);
+const allProduct = await ProductModel.find({});
+if (allProduct.length === 0) {
+  const answers = await inquirer.prompt({
+    type: "input",
+    name: "url",
+    message: "Please enter a url to continue",
   });
-
-  const latestProducts = Array.from(uniqueProducts.values());
-  console.log(latestProducts);
-  await Promise.all([
-    latestProducts.map(async (product: IProduct) => {
-      const products: IProduct[] = await getProductData(product?.link);
-      const selectedProduct: IProduct = products[0];
-      const createdProduct = await ProductModel.create({
-        ...selectedProduct,
-        link: product.link,
-      });
-      if (!createdProduct) {
-        console.log("Failed to add product data");
-      }
-      await new Promise((resolve) => setTimeout(resolve, 60000));
-    }),
-  ]);
-  return {
-    success: true,
-    message: "Successfully mined data",
-  };
-};
-productDataJob();
+  const url = answers.url.toString();
+  const products: IProduct[] = await getProductData(url);
+  const selectedProduct = products[0];
+  await ProductModel.create({
+    ...selectedProduct,
+    link: url,
+  });
+}
+cron.schedule("* * * * * *", async () => {
+  await productDataJob();
+});
---
+++ services/jobs/productDataJob.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { ProductModel, type IProduct } from "../../models/productModel";
+import { getProductData } from "../getProductData";
+import cron from "node-cron";
+import dotenv from "dotenv";
+dotenv.config();
+// we can use node cron to run this continuesly
+// cron.schedule("0 0 */12 * * *",);
+export const productDataJob = async () => {
+  const allProducts = await ProductModel.find();
+  console.log(allProducts);
+  if (!allProducts) {
+    return {
+      success: false,
+      message: "No product data found",
+    };
+  }
+  const uniqueProducts = new Map<string, any>();
+  allProducts.forEach((product: IProduct) => {
+    const key = `${product.name}-${product.price}`;
+    if (
+      !uniqueProducts.has(key) ||
+      new Date(product.createdAt) > new Date(uniqueProducts.get(key).createdAt)
+    ) {
+      uniqueProducts.set(key, product);
+    }
+  });
+
+  const latestProducts = Array.from(uniqueProducts.values());
+  console.log(latestProducts);
+  await Promise.all([
+    latestProducts.map(async (product: IProduct) => {
+      const products: IProduct[] = await getProductData(product?.link);
+      const selectedProduct: IProduct = products[0];
+      const createdProduct = await ProductModel.create({
+        ...selectedProduct,
+        link: product.link,
+      });
+      if (!createdProduct) {
+        console.log("Failed to add product data");
+      }
+      await new Promise((resolve) => setTimeout(resolve, 60000));
+    }),
+  ]);
+  return {
+    success: true,
+    message: "Successfully mined data",
+  };
+};
